4a5b82de104934e44dec35b3fc6d38282ffbd2b9
AnnKovalchukk <a.v.kovalchuk@student.csn.khai.edu>
2026-04-26 19:54:57 +0300

    docker_postgres laba_4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const productRoutes = require('./product.routes');
 const userRouter = require('./user/user.router');
-const { logRequest } = require('./middleware');
+const productRouter = require('./product/product.router')
+const {
+    logRequest
+} = require('./middleware');
 const bodyParser = require('body-parser');
-const { errorResponder } = require('./error.middleware');
+const {
+    errorResponder
+} = require('./error.middleware');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(logRequest);
+app.use(productRouter);
 app.use(productRoutes);
 app.use(userRouter);
 app.use(errorResponder);
 app.listen(PORT, () => {
- console.log(`Server listening at http://localhost:${PORT}`);
+    console.log(`Server listening at http://localhost:${PORT}`);
 });
